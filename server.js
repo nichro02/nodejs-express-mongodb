2b1fde1fcd02a38c4637a559aee3b37069d47967
mateenkazia <mateen.kazia@chaione.com>
2020-12-07 17:26:39 -0800

    added git ignore

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+// Express is for building the Rest apis
 const express = require("express");
+// body-parser helps to parse the request and create the req.body object
 const bodyParser = require("body-parser");
+// cors provides Express middleware to enable CORS with various options.
 const cors = require("cors");
 
 const app = express()
-
+/*
+Notice that we set origin: http://localhost:8081.
+– define a GET route which is simple for test.
+– listen on port 8080 for incoming requests.
+*/
 let corsOptions = {
     origin: "http://localhost:8081"
 }
-
 app.use(cors(corsOptions))
 
 // parse requests of content-type - application/json
@@ -28,3 +34,17 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
+
+/*
+###### The routes we will be creating! #########
+
+Methods  	Urls	Actions
+GET   api/tutorials	get all Tutorials
+GET	  api/tutorials/:id	get Tutorial by id
+POST  api/tutorials	add new Tutorial
+PUT	  api/tutorials/:id	update Tutorial by id
+DELETE	api/tutorials/:id	remove Tutorial by id
+DELETE	api/tutorials	remove all Tutorials
+GET	   api/tutorials/published	find all published Tutorials
+GET	   api/tutorials?title=[kw]	find all Tutorials which title contains 'kw'
+*/
